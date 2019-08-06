a2399782626a11b5e19202b02ac88625f567f2bf
Marko Martonosi <m.martonosi@vegaitsourcing.rs>
2019-08-06 10:39:10 +0200

    Fix pathing issues

--- commit_msg_hook/check_mm_commit_message.py
+++ commit_msg_hook/check_mm_commit_message.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__))))
 
@@ -9,13 +10,23 @@ def get_allowed_words_as_tuple():
         verbs = f.readlines()
         for verb in verbs:
             retval.append(verb.strip("\n"))
-
         return retval
+
+
+def get_git_dir():
+    git_dir_name = ".git"
+    working_dir = os.getcwd()
+    while working_dir != "/" and git_dir_name not in os.listdir(working_dir):
+        working_dir = os.path.abspath(working_dir + "/../")
+    os.chdir(f"{working_dir}/.git/")
+    return os.path.abspath(os.getcwd())
 
 
 def main(argv=None):
     retval = 0
-    git_hooks_dir = os.path.join(os.getcwd(), ".git", "hooks")
+
+    git_dir = get_git_dir()
+    git_hooks_dir = os.path.join(git_dir, "hooks")
     git_hooks_file = os.path.join(git_hooks_dir, "commit-msg.sample")
 
     try:
@@ -29,6 +40,7 @@ def main(argv=None):
             f.write("".join(script_content))
 
         new_file = os.path.join(git_hooks_dir, "commit-msg")
+        print(f"Commit-msg hooks successfully installed in directory: {new_file}")
         os.rename(git_hooks_file, new_file)
         os.system(f"chmod +x {new_file}")
     except Exception as e:
